87fd7ee95181af2bcf555963e906006fd562556e
Hiren Hiranandani <hhiranan@gmail.com>
2021-02-13 01:52:35 -0800

    [fix] make shellcheck happy about color variables

--- lib/utils.sh
+++ lib/utils.sh
@@ -1,29 +1,38 @@
 #!/usr/bin/env bash
-# shellcheck disable=2155
 
-if [ -z "$COLOR_SUPPORT" ]; then
-  export COLOR_SUPPORT="YES"
+DARK_GRAY="$(tput setaf 0)"
+RED="$(tput setaf 1)"
+GREEN="$(tput setaf 2)"
+YELLOW="$(tput setaf 3)"
+BLUE="$(tput setaf 4)"
+MAGENTA="$(tput setaf 5)"
+CYAN="$(tput setaf 6)"
+WHITE="$(tput setaf 7)"
+GRAY="$(tput setaf 8)"
+BOLD="$(tput bold)"
+UNDERLINE="$(tput sgr 0 1)"
+INVERT="$(tput sgr 1 0)"
+NORMAL="$(tput sgr0)"
 
-  export DARK_GRAY="$(tput setaf 0)"
-  export RED="$(tput setaf 1)"
-  export GREEN="$(tput setaf 2)"
-  export YELLOW="$(tput setaf 3)"
-  export BLUE="$(tput setaf 4)"
-  export MAGENTA="$(tput setaf 5)"
-  export CYAN="$(tput setaf 6)"
-  export WHITE="$(tput setaf 7)"
-  export GRAY="$(tput setaf 8)"
-  export BOLD="$(tput bold)"
-  export UNDERLINE="$(tput sgr 0 1)"
-  export INVERT="$(tput sgr 1 0)"
-  export NORMAL="$(tput sgr0)"
+export DARK_GRAY
+export RED
+export GREEN
+export YELLOW
+export BLUE
+export MAGENTA
+export CYAN
+export WHITE
+export GRAY
+export BOLD
+export UNDERLINE
+export INVERT
+export NORMAL
 
-  # Mispellings
-  export DARKGRAY="$DARK_GRAY"
-  export DARK_GREY="$DARK_GRAY"
-  export DARKGREY="$DARK_GRAY"
-  export GREY="$GRAY"
-fi
+# Mispellings
+export DARKGRAY="$DARK_GRAY"
+export DARK_GREY="$DARK_GRAY"
+export DARKGREY="$DARK_GRAY"
+export GREY="$GRAY"
 
 quote_params() {
   if [ "$#" -eq 0 ]; then
