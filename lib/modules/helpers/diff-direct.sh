87d19bde3072ae234b04b7934503f9f48fab16dc
Hiren Hiranandani <hhiranan@gmail.com>
2022-05-12 17:22:42 -0700

    strip everything after `--` for use in preview

--- lib/modules/helpers/diff-direct.sh
+++ lib/modules/helpers/diff-direct.sh
@@ -18,13 +18,27 @@ gf_helper_diff_direct_preview_content() {
   FILE_PATH="$1"
   shift
 
+  FOUND=
+  for INDEX_OF_ARG in $(seq 0 $#); do
+    VALUE="${!INDEX_OF_ARG}"
+    if [ "$VALUE" = '--' ]; then
+      FOUND=yes
+      break
+    fi
+  done
+
+  ARGS_TO_PASS=("$@")
+  if [ -n "$FOUND" ]; then
+    ARGS_TO_PASS=("${@:1:$((INDEX_OF_ARG - 1))}")
+  fi
+
   if [ -z "$FILE_PATH" ]; then
     echo "nothing to show"
   elif [ -z "$QUERY" ]; then
     # shellcheck disable=2086,2090
-    gf_git_command_with_header_default_parameters 1 "$GF_DIFF_FILE_PREVIEW_DEFAULTS" diff "$@" -- "$FILE_PATH" | gf_diff_renderer
+    gf_git_command_with_header_default_parameters 1 "$GF_DIFF_FILE_PREVIEW_DEFAULTS" diff "${ARGS_TO_PASS[@]}" -- "$FILE_PATH" | gf_diff_renderer
   else
     # shellcheck disable=2086,2090
-    gf_git_command_with_header_default_parameters 1 "$GF_DIFF_FILE_PREVIEW_DEFAULTS" diff "$@" -- "$FILE_PATH" | gf_diff_renderer | grep --color=always -E "$QUERY|$"
+    gf_git_command_with_header_default_parameters 1 "$GF_DIFF_FILE_PREVIEW_DEFAULTS" diff "${ARGS_TO_PASS[@]}" -- "$FILE_PATH" | gf_diff_renderer | grep --color=always -E "$QUERY|$"
   fi
 }
