31512bcd29d78fc0dbe1be10dfcf9797d363c1f7
Hiren Hiranandani <hhiranan@gmail.com>
2022-05-16 12:24:51 -0700

    support user-configured colors

--- lib/utils.sh
+++ lib/utils.sh
@@ -1,18 +1,18 @@
 #!/usr/bin/env bash
 
-DARK_GRAY="$(tput setaf 0)"
-RED="$(tput setaf 1)"
-GREEN="$(tput setaf 2)"
-YELLOW="$(tput setaf 3)"
-BLUE="$(tput setaf 4)"
-MAGENTA="$(tput setaf 5)"
-CYAN="$(tput setaf 6)"
-WHITE="$(tput setaf 7)"
-GRAY="$(tput setaf 8)"
-BOLD="$(tput bold)"
-UNDERLINE="$(tput sgr 0 1)"
-INVERT="$(tput sgr 1 0)"
-NORMAL="$(tput sgr0)"
+DARK_GRAY="${DARK_GRAY:-$(tput setaf 0)}"
+RED="${RED:-$(tput setaf 1)}"
+GREEN="${GREEN:-$(tput setaf 2)}"
+YELLOW="${YELLOW:-$(tput setaf 3)}"
+BLUE="${BLUE:-$(tput setaf 4)}"
+MAGENTA="${MAGENTA:-$(tput setaf 5)}"
+CYAN="${CYAN:-$(tput setaf 6)}"
+WHITE="${WHITE:-$(tput setaf 7)}"
+GRAY="${GRAY:-$(tput setaf 8)}"
+BOLD="${BOLD:-$(tput bold)}"
+UNDERLINE="${UNDERLINE:-$(tput sgr 0 1)}"
+INVERT="${INVERT:-$(tput sgr 1 0)}"
+NORMAL="${NORMAL:-$(tput sgr0)}"
 
 export DARK_GRAY
 export RED
